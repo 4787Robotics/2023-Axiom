af844f4b8cc3919020c9d0afc6a8d363c9af8261
4787 Robotics <>
2023-01-10 16:25:37 -0600

    Jonah - Small Fixes, not done

--- 2023Axiom/src/main/java/frc/robot/subsystems/MotorController.java
+++ 2023Axiom/src/main/java/frc/robot/subsystems/MotorController.java
@@ -7,24 +7,36 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import com.ctre.phoenix.motorcontrol.TalonFXInvertType;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 
-public class MotorController extends SubsystemBase {
-  private WPI_TalonFX m_arm, m_leftGrip, m_rightGrip;
-  // Creates and initializes motor controllers.
-  public MotorController() {
-    m_arm = new WPI_TalonFX(Constants.motor_arm);
-    m_leftGrip = new WPI_TalonFX(Constants.motor_leftGrip);
-    m_rightGrip = new WPI_TalonFX(Constants.motor_rightGrip);
+public class UMotorController extends SubsystemBase {
+  // Big Arm Motor
+  private m_arm = new WPI_TalonFX(Constants.motor_arm);
+  // Lower Arm clamp motor
+  private m_clamp = new WPI_TalonFX(Constants.MOTOR_CLAMP);
 
-    m_leftGrip.setInverted(TalonFXInvertType.Clockwise); // Makes left motor go the opposite of the right motor
-    
-    m_arm.setNeutralMode(NeutralMode.Brake); // Keeps the motors in place and stops them from moving without input
-    m_leftGrip.setNeutralMode(NeutralMode.Brake);
-    m_rightGrip.setNeutralMode(NeutralMode.Brake);
+  // Controls wheels that suck em up
+  private m_left_wheel = new WPI_TalonFX(Constants.MOTOR_LEFT_WHEEL);
+  private m_right_wheel = new WPI_TalonFX(Constants.MOTOR_RIGHT_WHEEL);
 
-    m_rightGrip.setSelectedSensorPosition(0); // for encoder
+  public UMotorController(double[] voltages) {
+    // Makes left motor go the opposite of the right motor
+    m_left.setInverted(TalonFXInvertType.Clockewise); 
+     // ewqeKeeps the motors in place and stops them frowm moving without input
+    m_arm.setNeutralMode(NeutralMode.Brake);
+    m_left_wheel.setNeutralMode(NeutralMode.Brake);
+    m_right_wheel.setNeutralMode(NeutralMode.Brak
+    m_clamp.setNeutralMode(NeutralMode.Brake);
+  
+
+     // For encoder
+    m_rightGrip.setSelectedSensorPosition(0);
     m_leftGrip.setSelectedSensorPosition(0);
 
-    m_leftGrip.configOpenloopRamp(0.4); // limits acceleration, takes 0.4 seconds to accelerate from 0 to 100%
+    // limits acceleration, takes 0.4 seconds to accelerate from 0 to 100%
+    m_leftGrip.configOpenloopRamp(0.4); 
     m_rightGrip.configOpenloopRamp(0.4); // may need change in the future when robot is active
+  }
+
+  public void clamp() {
+    m_leftGrip.
   }
 }
