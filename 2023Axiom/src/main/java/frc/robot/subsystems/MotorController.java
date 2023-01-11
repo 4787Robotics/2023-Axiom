8ffb085e3b49efe3e4835b764a6ac01c5d5faa42
JebJonah <115095915+JebJonah@users.noreply.github.com>
2023-01-11 17:32:35 -0600

    Jonah - Finished first Arm
    Added brushless, brake, and accel for the arm motor. I also added a to-do list that should be done (almost in order) once we know what motors we are using.

--- 2023Axiom/src/main/java/frc/robot/subsystems/MotorController.java
+++ 2023Axiom/src/main/java/frc/robot/subsystems/MotorController.java
@@ -4,6 +4,7 @@ import frc.robot.Constants;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 
@@ -25,22 +26,34 @@ public class WMotorController extends SubsystemBase {
     // Makes left motor go the opposite of the right motor
     m_left_wheel.setInverted(TalonFXInvertType.Clockwise); 
      // ewqeKeeps the motors in place and stops them frowm moving without input
-    m_arm.setNeutralMode(NeutralMode.Brake);
+    m_arm.setIdleMode(IdleMode.kBrake);
     m_left_wheel.setNeutralMode(NeutralMode.Brake);
     m_right_wheel.setNeutralMode(NeutralMode.Brake);
     m_clamp.setNeutralMode(NeutralMode.Brake);
   
 
      // For encoder
+     //I have no idea how to make one for SparkMax
     m_left_wheel.setSelectedSensorPosition(0);
     m_right_wheel.setSelectedSensorPosition(0);
 
     // limits acceleration, takes 0.4 seconds to accelerate from 0 to 100%
+    m_arm.setOpenLoopRampRate(0.4); //no idea if this one works
     m_left_wheel.configOpenloopRamp(0.4); 
     m_right_wheel.configOpenloopRamp(0.4); // may need change in the future when robot is active
 }
 
   public void clamp() {
-    
+     //Unused for now
   }
 }
+
+/**Changes needed but can't be done rn:
+ * 
+ * Figure out the motors for the clamp and wheels.
+ * Install the new libraries for each new type of motor.
+ * Use the motor types for the braking, Accel, etc.
+ *
+ * Add a following version of m_arm when for each new arm motor.
+ * 
+*/
