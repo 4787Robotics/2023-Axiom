fe6c227344eba321e3f879b5a0345c03afbc8c76
4787 Robotics <117868750+CabbageOfCato@users.noreply.github.com>
2023-01-11 16:17:38 -0600

    Added Fidel's UserInput Class to DriveCommand
    Saving before starting Autonomous

--- 2023Axiom/src/main/java/frc/robot/commands/DriveCommand.java
+++ 2023Axiom/src/main/java/frc/robot/commands/DriveCommand.java
@@ -10,6 +10,9 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.MotorSafety;
+import frc.robot.CXbox;
+import frc.robot.CJoystick;
+import frc.robot.Constants;
 
 import java.util.concurrent.TimeUnit;
 
@@ -19,5 +22,59 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 
 public class DriveCommand extends CommandBase {
-    
+    @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+  private final DriveTrain m_subsystem;
+  XboxController controller;
+  private WPI_TalonFX m_left1, m_right2;
+  private WPI_TalonFX m_left2, m_right1;
+  DifferentialDrive drive;
+  /**
+   * Creates a new ExampleCommand.
+   *
+   * @param subsystem The subsystem used by this command.
+   */
+  public DriveCommand(DriveTrain subsystem, XboxController inputController) {
+        controller = inputController;
+        m_subsystem = subsystem;
+        
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(subsystem);
+  }
+
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    if(CXbox.getRightStickXWithDeadzone() > Constants.rightStickDeadzone) {
+      m_subsystem.turnRight(controller.getRightX());
+    } else if (CXbox.getRightStickXWithDeadzone() < -Constants.rightStickDeadzone) {
+      m_subsystem.turnLeft(controller.getRightX());
+    }
+    if(CXbox.getLeftStickYWithDeadzone() < -Constants.leftStickDeadzone) {
+      m_subsystem.moveForward(controller.getLeftY());
+    } else if (CXbox.getRightStickYWithDeadzone() > Constants.rightStickDeadzone) {
+      m_subsystem.moveBackward(controller.getLeftY());
+    }
+    else {
+      feed();
+    }                                                                                                                                 
+  }
+
+  private void feed() {
+  }
+
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
 }
