ab3c53abdc9f43fd2829ef36b42eae47a6734934
4787 Robotics <4787software@gmail.com>
2023-01-26 16:27:22 -0600

    Added Arm Command, made some code better

--- 2023Axiom/src/main/java/frc/robot/subsystems/MotorController.java
+++ 2023Axiom/src/main/java/frc/robot/subsystems/MotorController.java
@@ -1,30 +1,46 @@
 package frc.robot.subsystems;
 
 import frc.robot.Constants;
+import frc.robot.CXbox; //IDK if this works
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
-import com.ctre.phoenix.motorcontrol.TalonFXInvertType;
-import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType; 
 
-public class MotorController extends SubsystemBase {
-  private WPI_TalonFX m_arm, m_leftGrip, m_rightGrip;
-  // Creates and initializes motor controllers.
-  public MotorController() {
-    m_arm = new WPI_TalonFX(Constants.MOTOR_ARM);
-    m_leftGrip = new WPI_TalonFX(Constants.MOTOR_LEFT_GRIP);
-    m_rightGrip = new WPI_TalonFX(Constants.MOTOR_RIGHT_GRIP);
+public class MotorControllezzr extends SubsystemBase {
+  // Big Arm Motor
+  private CANSparkMax m_arm = new CANSparkMax(Constants.MOTOR_ARM, MotorType.kBrushless);
+  // Lower Arm clamp motor
+  private WPI_TalonFX m_clamp = new WPI_TalonFX(Constants.MOTOR_CLAMP);
 
-    m_leftGrip.setInverted(TalonFXInvertType.Clockwise); // Makes left motor go the opposite of the right motor
+  // Controls wheels that suck em up
+  private WPI_TalonFX m_left_wheel = new WPI_TalonFX(Constants.MOTOR_LEFT_WHEEL);
+  private WPI_TalonFX m_right_wheel = new WPI_TalonFX(Constants.MOTOR_RIGHT_WHEEL);
+
+  public WMotorController(double[] voltages) {
+    if (voltages.length != 4) {
+      System.out.println("Need 4 arguments");
+      return;
+    }
+    // Makes left motor go the opposite of the right motor
+    m_left_wheel.setInverted(TalonFXInvertType.Clockwise); 
+     // ewqeKeeps the motors in place and stops them frowm moving without input
+    m_arm.setNeutralMode(NeutralMode.Brake);
+    m_left_wheel.setNeutralMode(NeutralMode.Brake);
+    m_right_wheel.setNeutralMode(NeutralMode.Brake);
+    m_clamp.setNeutralMode(NeutralMode.Brake);
+  
+
+     // For encoder
+    m_left_wheel.setSelectedSensorPosition(0);
+    m_right_wheel.setSelectedSensorPosition(0);
+
+    // limits acceleration, takes 0.4 seconds to accelerate from 0 to 100%
+    m_left_wheel.configOpenloopRamp(0.4); 
+    m_right_wheel.configOpenloopRamp(0.4); // may need change in the future when robot is active
+}
+
+  public void clamp() {
     
-    m_arm.setNeutralMode(NeutralMode.Brake); // Keeps the motors in place and stops them from moving without input
-    m_leftGrip.setNeutralMode(NeutralMode.Brake);
-    m_rightGrip.setNeutralMode(NeutralMode.Brake);
-
-    m_rightGrip.setSelectedSensorPosition(0); // for encoder
-    m_leftGrip.setSelectedSensorPosition(0);
-
-    m_leftGrip.configOpenloopRamp(0.4); // limits acceleration, takes 0.4 seconds to accelerate from 0 to 100%
-    m_rightGrip.configOpenloopRamp(0.4); // may need change in the future when robot is active
   }
 }
