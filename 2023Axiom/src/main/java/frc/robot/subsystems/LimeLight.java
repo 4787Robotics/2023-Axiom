cc26f298e56fc7ee5e9fc0423dc8ab82f1b03102
SatisfiedSushi <71732695+SatisfiedSushi@users.noreply.github.com>
2023-02-01 16:27:19 -0600

    Update LimeLight.java

--- 2023Axiom/src/main/java/frc/robot/subsystems/LimeLight.java
+++ 2023Axiom/src/main/java/frc/robot/subsystems/LimeLight.java
@@ -5,33 +5,44 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
+import frc.robot.Constants;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 
 public class LimeLight extends SubsystemBase {
-  /** Creates a new ExampleSubsystem. */
+  //NetworkTable fields
   NetworkTable table;
+  NetworkTableEntry tv;
   NetworkTableEntry tx;
   NetworkTableEntry ty;
   NetworkTableEntry ta;  
+  NetworkTableEntry tid;
+  NetworkTableEntry getpipe;
+  NetworkTableEntry pipeline;
 
+  //Other fields
   double distance = 0;
 
+  //Initialize fields and get NetworkTable for the first limelight
   public LimeLight() {
     table = NetworkTableInstance.getDefault().getTable("limelight");
+    tv = table.getEntry("tv");
     tx = table.getEntry("tx");
     ty = table.getEntry("ty");
     ta = table.getEntry("ta");
+    tid = table.getEntry("tid");
+    getpipe = table.getEntry("getpipe");
+    pipeline = table.getEntry("pipeline");
   }
 
-  public double getX() {
+  //Returns the horizontal angle the detected object 
+  public double getXAngle() {
     return tx.getDouble(0.0);
   }
 
-  public double getY() {
+  public double getYAngle() {
     return ty.getDouble(0.0);
   }
 
@@ -39,9 +50,38 @@ public class LimeLight extends SubsystemBase {
     return ta.getDouble(0.0);
   }
 
+  public double getTagID() {
+    return tid.getDouble(0.0);
+  }
+
+  public double getPipline() {
+    return getpipe.getDouble(0.0);
+  }
+
+  public boolean hasTarget() {
+    if (tv.getDouble(0.0) == 1) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  /** 
+   * Set pipline number between 0-9.
+  */
+  public void setPipeline(double pipelineNumber) {
+    pipeline.setValue(pipelineNumber);
+  }
+  
+
+  public void setTargetedAprilTagId(double targetedAprilTagId) {
+    setPipeline(targetedAprilTagId + 1);
+  }
+
   public double calculateDistance() {
-    distance = 0;
-    double area = getArea();
+
+    //with area (doesnt fucking work im ass mb)
+    /*double area = getArea();
     if (area < 1) {
       distance = -1; //-1 means the limelight does not see a target
     } else {  //calculate the distance based on the area of the target and some constants that we found experimentally.  This is not perfect, but it works for our robot.  
@@ -53,27 +93,33 @@ public class LimeLight extends SubsystemBase {
 
        } 
 
-       return 0;   //this should never happen because we check for tv=0 above and return -1 if true, but just in case...
+       return 0;*/
+    
+    //using known variables (angles, heights, etc.)
+    double area = getArea();
+    if (area == 0) {
+      distance = -1; //-1 means the limelight does not see a target
+    } else {  //calculate the distance based on the area of the target and some constants that we found experimentally.  This is not perfect, but it works for our robot. 
+      double angleToGoalDegrees = Constants.LimelightMountAngle + getYAngle();
+      double angleToGoalRadians = angleToGoalDegrees * (3.14159 / 180.0); 
+
+      distance = (Constants.GoalHeight - Constants.LimelightLensHeight)/Math.tan(angleToGoalRadians);   
+    } 
+    return distance; //returns calculated distance in inches from limelight to reflective tape target or -1 if no valid targets are seen by Limelight camera (i.e., tv=0)
   }
 
   public void updateDashboard() {
-    SmartDashboard.putNumber("Distance", distance);
-    SmartDashboard.putNumber("LimelightX", getX());
-    SmartDashboard.putNumber("LimelightY", getY());
+    SmartDashboard.putNumber("Distance", calculateDistance());
+    SmartDashboard.putNumber("LimelightX", getXAngle());
+    SmartDashboard.putNumber("LimelightY", getYAngle());
     SmartDashboard.putNumber("LimelightArea", getArea());
-    /*System.out.println("LimelightX" + Double.toString(getX()));
-    System.out.println("LimelightY" + Double.toString(getY()));
-    System.out.println("LimelightArea" + Double.toString(getArea()));*/
+    SmartDashboard.putNumber("CurrentTargetedTagID", getTagID());
+    SmartDashboard.putNumber("CurrentPipline", getPipline());
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-    SmartDashboard.putNumber("LimelightX", getX());   //puts the Limelight's x-axis offset of crosshair from center of target in degrees on the SmartDashboard so we can see it on the Driver Station and tune our aiming code to keep this value as close to zero as possible when targeting reflective tape targets.  
-
-    SmartDashboard.putNumber("LimelightY", getY());   //puts the Limelight's y-axis offset of crosshair from center of target in degrees on the SmartDashboard so we can see it on the Driver Station and tune our aiming code to keep this value as close to zero as possible when targeting reflective tape targets.  
-
-    SmartDashboard.putNumber("LimelightArea", getArea());     // puts ta (target area) reported by Limeligt camera for current frame on Shart Dashbaord so we can use these values with actual measured distances between limeligh camera and target at different angles relative to camera lens axis, then curve fit those data points using a graphing utility like Excel or Google Sheets to come up with a formula that calculates distance from Limelight camera to target based on ta value reported by Limelight for current frame.  This is not perfect, but it works well enough for our robot.  
   }
 
   @Override
