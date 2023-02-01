b55fe056ed92fdd0335af95ebe7198ecfffb5cb6
DetermiedMech1 <determiedmech1@gmail.com>
2023-02-01 17:50:35 -0600

    removed testing print commands and fixed my issue with commiting onto main :p (sry about that)

--- 2023Axiom/src/main/java/frc/robot/Robot.java
+++ 2023Axiom/src/main/java/frc/robot/Robot.java
@@ -8,6 +8,7 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.subsystems.Balance;
+import frc.robot.subsystems.Scorekeeper;
 //import frc.robot.subsystems.Scorekeeper;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import com.kauailabs.navx.frc.AHRS;
@@ -154,6 +155,6 @@ public class Robot extends TimedRobot {
     CJoystick.joystickButton11Down();
     CJoystick.joystickButton12Down();
     */
-    //Scorekeeper.updateDashboard();
+    Scorekeeper.updateDashboard();
   }
 }
