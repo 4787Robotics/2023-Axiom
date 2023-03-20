2face92a89ac883e9ae725a081b8df7f18cec7ae
Alex <atdanakis@gmail.com>
2023-03-20 17:46:41 -0500

    Got autocommand working

--- 2023Axiom/src/main/java/frc/robot/commands/AutoMovements.java
+++ 2023Axiom/src/main/java/frc/robot/commands/AutoMovements.java
@@ -7,23 +7,33 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.DriveTrain;
 import edu.wpi.first.wpilibj.Timer;
+import frc.robot.subsystems.MotorController;
 
-public class DriveForwardAuto extends CommandBase {
+public class AutoMovements extends CommandBase {
   /** Creates a new DriveForwardAuto. */
   DriveTrain driveTrain;
+  MotorController motorController;
   double timeStarted;
   boolean isFinished;
   boolean isForward;
-  public DriveForwardAuto(DriveTrain m_driveTrain) {
+  boolean isSlamming;
+
+  public AutoMovements(DriveTrain m_driveTrain) {
     // Use addRequirements() here to declare subsystem dependencies.
     driveTrain = m_driveTrain;
     isForward = true;
+    isSlamming = false;
   }
 
-  public DriveForwardAuto(DriveTrain m_driveTrain, boolean m_isForward) {
+  public AutoMovements(DriveTrain m_driveTrain, boolean m_isForward) {
     // Use addRequirements() here to declare subsystem dependencies.
     driveTrain = m_driveTrain;
     isForward = m_isForward;
+  }
+
+  public AutoMovements(MotorController m_motorController) {
+    motorController = m_motorController;
+    isSlamming = true;
   }
 
   // Called when the command is initially scheduled.
@@ -35,22 +45,34 @@ public class DriveForwardAuto extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    if (isForward == true) {
+    if (isSlamming) { //Slamming arm auto
       if (timeStarted == 0) {
         timeStarted = Timer.getFPGATimestamp();
       }
-      driveTrain.driveRobot(false, 0.8, 0);
-      if (Timer.getFPGATimestamp() > (timeStarted + 0.5)) {
+      motorController.ArmMove(-0.3);
+      if (Timer.getFPGATimestamp() > (timeStarted + 1.2)) {
         driveTrain.driveRobot(false, 0, 0);
         this.cancel();
         isFinished = true;
       }
-    } else {
+    }
+    else if (isForward == true) { //Moving forward auto
+      if (timeStarted == 0) {
+        timeStarted = Timer.getFPGATimestamp();
+      }
+      driveTrain.driveRobot(false, 0.8, 0);
+      if (Timer.getFPGATimestamp() > (timeStarted + 1)) {
+        driveTrain.driveRobot(false, 0, 0);
+        this.cancel();
+        isFinished = true;
+      }
+
+    } else { //Moving backward auto
       if (timeStarted == 0) {
         timeStarted = Timer.getFPGATimestamp();
       }
       driveTrain.driveRobot(false, -0.75, 0);
-      if (Timer.getFPGATimestamp() > (timeStarted + 0.3)) {
+      if (Timer.getFPGATimestamp() > (timeStarted + 0.7)) {
         driveTrain.driveRobot(false, 0, 0);
         this.cancel();
         isFinished = true;
